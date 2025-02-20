64170fd7648e23ab06063d0a004052be3ccae770
Sara Kleppe <saralkleppe@gmail.com>
2025-02-20 17:04:52 +0100

    Add functioning randomisation of background color, leaf color and branch color

--- sketch.js
+++ sketch.js
@@ -1,75 +1,88 @@
-const golden_ratio = 0.618033988749;
-const golden_ratio_exp3 = golden_ratio * golden_ratio * golden_ratio;
-const golden_ratio_exp4 = golden_ratio_exp3 * golden_ratio;
+const goldenRatio = 0.618033988749;
+const goldenRatioExp3 = goldenRatio * goldenRatio * goldenRatio;
+const goldenRatioExp4 = goldenRatioExp3 * goldenRatio;
 let trees = [];
+let backgroundColor;
 
 function setup() {
-    createCanvas(300, 400);
+  createCanvas(500, 500);
+  generateBackgroundColor(); // Initialize background color using a function
 }
 
 function draw() {
-  background(200, 40, 100);
-  
-  fill(100, 40, 50);
+  background(backgroundColor[0], backgroundColor[1], backgroundColor[2]);
+
+  fill(backgroundColor[2], backgroundColor[0], backgroundColor[1]);
   noStroke();
-  rect(0, height * golden_ratio, width, height - height * golden_ratio)
-  
-  
-  let branch_size = height * golden_ratio_exp3;
-  
-  let stroke_weight = golden_ratio_exp4 * branch_size;
-  
-  strokeWeight(stroke_weight);
-  
-  //for(let i = 0; i < trees.length; i++)
-    //let pos = trees[i];
-    //circle(pos.x, pos.y, 20);
-    for(const treeInfo of trees){
-      
+  rect(0, height * goldenRatio, width, height - height * goldenRatio);
+
+  let branchSize = height * goldenRatioExp3;
+  let strokeWeightValue = goldenRatioExp4 * branchSize;
+  strokeWeight(strokeWeightValue);
+
+   // Sort trees by y-position (higher y = closer, should be drawn last)
+   trees.sort((a, b) => a.y - b.y);
+
+  for (const treeInfo of trees) {
     let distance = treeInfo.y / height;
-      distance = distance * distance;
-    
+    distance = distance * distance;
+
     fill(100, 170 * distance, 70);
     const color = treeInfo.color;
-  
-    stroke(color[0], color[1], color[2], 255);
-    
+
+    stroke(color[1], color[0], color[2]);
+
     push();
     translate(treeInfo.x, treeInfo.y);
-    branch(branch_size * distance, treeInfo.phase);
+    branch(branchSize * distance, treeInfo.phase, treeInfo.leafColor);
     pop();
   }
 }
 
-function branch(branch_size, phase){
-  if (branch_size > 3){
-    let stroke_weight = golden_ratio_exp4 * branch_size;
-    strokeWeight(stroke_weight);
-    
-    line(0, 0, 0, -branch_size);
-    
-    translate(0, -branch_size);
-    
+function branch(branchSize, phase, leafColor) {
+  if (branchSize > 3) {
+    let strokeWeightValue = goldenRatioExp4 * branchSize;
+    strokeWeight(strokeWeightValue);
+
+    line(0, 0, 0, -branchSize);
+    translate(0, -branchSize);
+
     const angle = frameCount * 0.01 + phase;
     const wind = 0.1;
 
     push();
     rotate(QUARTER_PI + sin(angle) * wind);
-    branch(branch_size * golden_ratio, phase);
+    branch(branchSize * goldenRatio, phase, leafColor);
     pop();
+    
     push();
     rotate(-QUARTER_PI + cos(angle) * wind * 0.5);
-    branch(branch_size * golden_ratio, phase);
+    branch(branchSize * goldenRatio, phase, leafColor);
     pop();
-  }
-  else{
+  } 
+  else {
     noStroke();
+    fill(leafColor[0], leafColor[1], leafColor[2]);
     circle(0, 0, 4);
   }
 }
 
-function mousePressed(){
-  const c = [random(0, 255), random(0, 255), random(0, 255)];
-  let treeInfo = {x: mouseX, y: mouseY, phase: random(-PI, PI), color: c};
+function mousePressed() {
+  generateBackgroundColor(); // Update background color when clicking
+  const color = [random(0, 255), random(0, 255), random(0, 255)];
+  const leafColor = [random(50, 200), random(100, 255), random(50, 200)];
+  
+  let treeInfo = { 
+    x: mouseX, 
+    y: mouseY, 
+    phase: random(-PI, PI), 
+    color: color, 
+    leafColor: leafColor 
+  };
+  
   trees.push(treeInfo);
+}
+
+function generateBackgroundColor() {
+  backgroundColor = [random(0, 255), random(0, 255), random(0, 255)];
 }
